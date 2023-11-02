ed1c8c28b827e6f1f204a8efef5d0192a67168e4
Asmae Hariti <haritiasmaa74@gmail.com>
2023-11-02 23:53:57 +0100

    adding the dynamic data to dropdown menu

---
+++ js/basket2.js
@@ -0,0 +1,28 @@
+const STORAGE_KEY = 'basket';
+
+let basket = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+let basketCountElement = document.getElementById('basket-count');
+basketCountElement.textContent = basket.length;
+
+function incrementBasket(details) {
+  basket.push(details);
+  basketCountElement.textContent = basket.length;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(basket));
+}
+
+
+const rentButtons = document.querySelectorAll('#but1');
+rentButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    let productContainer = button.closest('.product');
+    let name = productContainer.querySelector('#carName').textContent.trim();
+    let price = productContainer.querySelector('#carPrice').textContent.trim();
+    let image = productContainer.querySelector('img').src;
+    let details = {
+      name,
+      price,
+      image,
+    };
+    incrementBasket(details);
+  });
+});
--- js/dropDown.js
+++ js/dropDown.js
@@ -1,16 +1,48 @@
-function myFunction() {
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("show"); //adding a class show
+// }
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function (event) {
+//   if (!event.target.matches(".dropbtn")) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains("show")) {
+//         openDropdown.classList.remove("show");
+//       }
+//     }
+//   }
+// };
+
+function toggleDropdown() {
   document.getElementById("myDropdown").classList.toggle("show"); //adding a class show
 }
-// Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
+
+function renderBasket(dropdown, basket) {
+  dropdown.innerHTML = basket
+    .map((car) => {
+      return `
+            <img src="${car.image}"/>
+            <a class="text-black py-5  no-underline block">${car.name}</a>
+            <a class="text-black py-5 no-underline block">${car.price}</a>
+          `;
+    })
+    .join("\n");
+}
+
+window.addEventListener("load", () => {
+  renderBasket(document.getElementById("myDropdown"), basket);
+});
+
+window.addEventListener("click", function (event) {
   if (!event.target.matches(".dropbtn")) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains("show")) {
-        openDropdown.classList.remove("show");
-      }
-    }
+    let dropdownContent = event.target
+      .closest(".dropdown")
+      .querySelector(".dropdown-content");
+    console.log(dropdownContent);
+    renderBasket(dropdownContent, basket);
+  } else {
+    // TODO: close dropdown on click outside
   }
-};
+});
